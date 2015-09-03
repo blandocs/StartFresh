234a18587248c2b36686e12268bbf1ba0b6d4c1f
HyunSuKim <gustnxodjs@snu.ac.kr>
2015-09-03 22:31:05 +0900

    Make info action and start make another activity RoomSetting

--- app/src/main/java/kr/ac/gustnxodjssnu/startfresh/MainActivity.java
+++ app/src/main/java/kr/ac/gustnxodjssnu/startfresh/MainActivity.java
@@ -1,12 +1,19 @@
 package kr.ac.gustnxodjssnu.startfresh;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "kr.ac.gustnxodjssnu.startfresh";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +36,37 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_info:
+                PopUpInfo();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, RoomSetting.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void PopUpInfo() {
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.info,null);
+
+        AlertDialog.Builder aDialog = new AlertDialog.Builder(this);
+        aDialog.setTitle("info"); //타이틀바 제목
+        aDialog.setView(layout); //info.xml 파일을 뷰로 셋팅
+
+        //그냥 닫기버튼을 위한 부분
+        aDialog.setNegativeButton("ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog info = aDialog.create();
+        info.show();
     }
 }
